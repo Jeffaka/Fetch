c9323703eb49aee76bac3ae5a220c9d782726e0f
Bohdan Pashko <boss.pashko27@gmail.com>
2019-09-14 21:51:04 +0300

    Fetch data from api and create posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Todo {
+  completed: boolean
+  id?: number
+  title: string
+  userId?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+  todos: Todo[] = []
   title = 'fetch';
+  todoTitle = ''
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(){
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+        .subscribe(todos =>{
+          console.log('Respons ', todos)
+          this.todos = todos
+        })
+  }
+  addTodo(){
+    if(!this.todoTitle.trim()){
+      return
+    }
+    const newTodo: Todo = {
+      title:this.todoTitle,
+      completed: false
+    }
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+        .subscribe(todo => {
+          console.log('todo', todo)
+          this.todos.push(todo)
+          this.todoTitle = ''
+        })
+  }
 }
